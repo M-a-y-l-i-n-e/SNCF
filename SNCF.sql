49e754f5b7995118a84b8693fea2f6a92aa0f9f5
M-a-y-l-i-n-e <58560768+M-a-y-l-i-n-e@users.noreply.github.com>
2020-01-21 14:12:32 +0100

    Update SNCF.sql

--- SNCF.sql
+++ SNCF.sql
@@ -57,13 +57,13 @@ CREATE TABLE IF NOT EXISTS `tgv` (
 -- Index pour la table `gare`
 --
 ALTER TABLE `gare`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY IF NOT EXISTS (`id`);
 
 --
 -- Index pour la table `tgv`
 --
 ALTER TABLE `tgv`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY IF NOT EXISTS (`id`),
   ADD KEY `garearriv` (`idgarearrivee`),
   ADD KEY `garedepart` (`idgaredepart`);
 
