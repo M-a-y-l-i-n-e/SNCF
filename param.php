0700899dc2e58b77d2c3ce37a880e6aa3ef307f6
M-a-y-l-i-n-e <58560768+M-a-y-l-i-n-e@users.noreply.github.com>
2020-01-21 15:08:49 +0100

    Create param.php

---
+++ param.php
@@ -0,0 +1,21 @@
+//// param.php
+
+<?php
+
+$server="localhost";
+$user="user";
+$pass="test";
+$db="SNCF";
+
+
+
+$link = mysql_connect($server , $user, $pass);
+if (!$link) {
+   die('Impossible de se connecter : ' . mysql_error());
+}
+
+// Rendre la base de données foo, la base courante
+$db_selected = mysql_select_db($db, $link);
+if (!$db_selected) {
+   die ('Impossible de sélectionner la base de données : ' . mysql_error());
+}
